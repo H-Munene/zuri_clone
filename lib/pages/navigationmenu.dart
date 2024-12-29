c817725ea5eabb3c86695d221030d18780b073a3
Hezekiah Munene <hezemunene01@gmail.com>
2024-12-29 21:50:42 +0300

    refactor: class renaming

--- lib/pages/navigationmenu.dart
+++ lib/pages/navigationmenu.dart
@@ -5,29 +5,27 @@ class NavigationMenu extends StatefulWidget {
   const NavigationMenu({super.key});
 
   @override
-  State<NavigationMenu> createState() => _HomepageState();
+  State<NavigationMenu> createState() => _NavigationMenuState();
 }
 
-class _HomepageState extends State<NavigationMenu> {
+class _NavigationMenuState extends State<NavigationMenu> {
   int currentPageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: NavigationBarTheme(
-        data: NavigationBarThemeData(
-          indicatorColor: Colors.lightBlueAccent),
-        child: NavigationBar(
-          onDestinationSelected: (index) {
-            setState(() {
-              currentPageIndex = index;
-            });
-          },
-          selectedIndex: currentPageIndex,
-          destinations: globals.bottomNavBar.map((item) => item).toList(),
+        bottomNavigationBar: NavigationBarTheme(
+          data: NavigationBarThemeData(indicatorColor: Colors.lightBlueAccent),
+          child: NavigationBar(
+            onDestinationSelected: (index) {
+              setState(() {
+                currentPageIndex = index;
+              });
+            },
+            selectedIndex: currentPageIndex,
+            destinations: globals.bottomNavBar.map((item) => item).toList(),
+          ),
         ),
-      ),
-      body: globals.pages[currentPageIndex]
-    );
+        body: globals.pages[currentPageIndex]);
   }
 }
